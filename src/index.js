34d08f478487279d367c518cea26a64db94e4a2d
yangzhengca <yangzhengca@gmail.com>
2021-09-20 13:26:46 -0600

    finished home, cryptocurrencies, and news page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
-import 'antd/dist/antd.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "antd/dist/antd.css";
+import { Provider } from "react-redux";
 
-import App from './App';
+import App from "./App";
+import store from "./app/store";
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
- , document.getElementById('root'));
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '72af9e42aemsha6fb9c4f81ed693p1c642ajsn581687a64ce7'
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
